b9e7929f14fbfb817d0a10565fbfae1e9359b144
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-16 16:11:48 +0530

    Evm

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [BJP, setBjp] = useState(0);
+  const [Congress, SetCongress] = useState(0);
+  const [Sp, setSp] = useState(0);
+  const [BSP, setBSP] = useState(0);
+
+  function Votecout() {
+    setBjp((prev) => prev + 1)
+  }
+  return (
+    <div className='flex flex-col  items-center justify-center pt-20 gap-5'>
+      <div className='flex gap-5 items-center'>
+        <div className='h-12 w-24 bg-orange-600 rounded-2xl p-3' onClick={() => Votecout()}>🪷 BJP</div>
+        <div>{BJP}</div>
+      </div>
+      <div className='flex gap-5 items-center'>
+        <div className='h-12 w-24 bg-blue-200 rounded-2xl p-3 text-[12px]' onClick={()=>Votecout()}>✋ Congress</div>
+        <div>{Congress}</div>
+      </div>
+      <div className='flex gap-5 items-center'>
+        <div className='h-12 w-24 rounded-2xl p-3 bg-red-700'onClick={()=>Votecout()} >🚲 Sp</div>
+        <div>{Sp}</div>
+      </div>
+      <div className='flex gap-5 items-center'>
+        <div className='h-12 w-24 rounded-2xl p-3 bg-blue-800'onClick={()=>Votecout()}>🐘 BSP</div>
+        <div>{BSP}</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
